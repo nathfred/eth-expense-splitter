a3ddb03ecbd774230d1e353e3bbe124bb55c7b66
Nathanael Fredericko <79194414+nathfred@users.noreply.github.com>
2025-03-25 21:49:57 +0700

    Create contract

---
+++ packages/hardhat/contracts/ExpenseSplitter.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract ExpenseSplitter {
+    address public owner;
+    mapping(address => uint256) public balances;
+    address[] public participants;
+
+    event ExpenseAdded(address indexed payer, uint256 amount);
+    event FundsWithdrawn(address indexed user, uint256 amount);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function addExpense(address[] memory _participants) public payable {
+        require(msg.value > 0, "Must send ETH");
+        uint256 share = msg.value / _participants.length;
+        for (uint256 i = 0; i < _participants.length; i++) {
+            balances[_participants[i]] += share;
+        }
+        emit ExpenseAdded(msg.sender, msg.value);
+    }
+
+    function withdrawFunds() public {
+        uint256 amount = balances[msg.sender];
+        require(amount > 0, "No funds to withdraw");
+        balances[msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
+        emit FundsWithdrawn(msg.sender, amount);
+    }
+}
